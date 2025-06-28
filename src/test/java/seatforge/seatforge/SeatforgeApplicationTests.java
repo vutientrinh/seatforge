467f377c813644bba0055acc5f254a4166e70218
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-06-28 21:04:55 +0700

    Initial commit

---
+++ src/test/java/seatforge/seatforge/SeatforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package seatforge.seatforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
