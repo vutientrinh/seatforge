851c263dc7846aaf24bad135ee25f40d1445f117
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-07 22:02:53 +0700

    userdetail service

---
+++ src/main/java/seatforge/seatforge/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package seatforge.seatforge.security.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import seatforge.seatforge.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity =
+                userRepository
+                        .findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("user " + username + " not found"));
+
+        Set<GrantedAuthority> authorities =
+                userEntity.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                        .collect(Collectors.toSet());
+
+        return UserDetailsImpl.builder()
+                .username(userEntity.getUsername())
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .password(userEntity.getPassword())
+                .authorities(authorities)
+                .id(userEntity.getId())
+                .build();
+    }
+}
