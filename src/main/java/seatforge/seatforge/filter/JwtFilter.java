12d7dc6cf6a6ab9dde87e2e743ad35ac97dffe44
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-07 22:02:27 +0700

    filter

--- src/main/java/seatforge/seatforge/filter/JwtFilter.java
+++ src/main/java/seatforge/seatforge/filter/JwtFilter.java
@@ -1,4 +1,18 @@
 package seatforge.seatforge.filter;
 
-public class JwtFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+import seatforge.seatforge.security.service.UserDetailsImpl;
+import seatforge.seatforge.utils.JwtUtils;
+
+@Slf4j
+@Configuration
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsService userDetailsService;
 }
