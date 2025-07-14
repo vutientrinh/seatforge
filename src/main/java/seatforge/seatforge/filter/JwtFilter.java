e9c786758a166a262b54c426766ff802ca79cb79
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-14 15:32:03 +0700

    config ( error )

--- src/main/java/seatforge/seatforge/filter/JwtFilter.java
+++ src/main/java/seatforge/seatforge/filter/JwtFilter.java
@@ -1,12 +1,23 @@
 package seatforge.seatforge.filter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 import seatforge.seatforge.security.service.UserDetailsImpl;
+import seatforge.seatforge.security.service.UserDetailsServiceImpl;
 import seatforge.seatforge.utils.JwtUtils;
+
+import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -15,4 +26,42 @@ public class JwtFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    public RestApiTokenFilter(JwtUtils jwtUtils, UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String jwt = null;
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null && token.length() > 6) {
+            jwt = token.substring(7);
+        }
+        return jwt;
+    }
 }
