d35425c66c94b89b01f9f752e584cd5c484daf4f
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-15 17:24:20 +0700

    seat controller

---
+++ src/main/java/seatforge/seatforge/controller/SeatController.java
@@ -0,0 +1,11 @@
+package seatforge.seatforge.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatController {
+}
